437f07099437a404f56d7fbe173b20d93ca660f3
Ran10211109 <washiarashi0708@outlook.jp>
2023-11-19 18:20:45 +0900

    complete

--- machine_epsilon.py
+++ machine_epsilon.py
@@ -1,10 +1,8 @@
 # TODO
 e = 1
 while 1 + e > 1:
-    e * 0.5
-    if e <= 0:
-        break
-    print(e)
+    e *= 0.5
+print(2 * e)
 
 
 
--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -3,5 +3,13 @@ from math import sin
 # print(sin(0))
 # >>> 0
 # -----------
+from math import pi
+a = 0
+b = pi / 2 
+n = 100
+h = (b -a) / n
+s = 0
+for k in range(1, n + 1):
+    s += h / 2 * (sin(a + (k - 1) * h) + sin(a + k * h))
 
-print(sin(θ))
+print(s)
